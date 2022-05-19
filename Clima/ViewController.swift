77727f388e8a5ec9d1d0fde68e91dd1da50ebd45
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-05-19 20:15:26 +0700

    Initial Commit

---
+++ Clima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Muhammad Ziddan Hidayatullah on 19/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
