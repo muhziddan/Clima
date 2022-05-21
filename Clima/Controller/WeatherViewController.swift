d39076256c6678d644010dafd867fa73a712380a
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-05-21 12:48:59 +0700

    Add method for the text field and change the search pressed from text field to button

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,17 +7,45 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
-
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
+        updateUI()
     }
-
-
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        updateUI()
+        if textField.text != "" {
+            return true
+        } else {
+            searchTextField.placeholder = "Type City"
+            return true
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(searchTextField.text ?? "")
+        searchTextField.text = ""
+    }
+    
+    func updateUI() {
+        searchTextField.placeholder = "Search City"
+    }
 }
-
