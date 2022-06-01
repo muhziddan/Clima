d7209b4f3a1f1a900081fac61f4aedb54c2b7bbf
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-06-01 14:02:21 +0700

    add getch function, made a URL object, made a URLSession, give the task to URLSession and start the task

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -8,10 +8,31 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=jakarta&appid=3beb2ead23c5bab59e6c5233f8cbf529&units=metric"
     
     func fetchCity(_ cityName: String) {
-        let url = "\(weatherURL)&q=\(cityName)"
-        print(url)
+        let url = "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=3beb2ead23c5bab59e6c5233f8cbf529&units=metric"
+        request(url)
+    }
+    
+    func request(_ urlInput: String) {
+        guard let url = URL(string: urlInput) else {return}
+        
+        let session = URLSession(configuration: .default)
+        
+        let task = session.dataTask(with: url, completionHandler: handler(data:response:error:))
+        
+        task.resume()// kenapa bahasanya resume? karena task ini sifatnya suspended/ditangguhkan same to pause, makanya method buat dimulainya namanya resume
+    }
+    
+    func handler(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error ?? "")
+            return
+        }
+        
+        guard let safeData = data else {return}
+        
+        let dataString = String(data: safeData, encoding: .utf8)
+        print(dataString ?? "")
     }
 }
